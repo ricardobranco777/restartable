243c669cef78909125640043c828d21ed138df90
Ricardo Branco <rbranco@suse.de>
2025-01-05 16:20:49 +0100

    Bump version

--- restartable.go
+++ restartable.go
@@ -20,7 +20,7 @@ import (
 
 import flag "github.com/spf13/pflag"
 
-const version string = "2.3.3"
+const version string = "2.3.4"
 
 // ProcFS defines an interface for /proc/ filesystem access
 type ProcFS interface {
