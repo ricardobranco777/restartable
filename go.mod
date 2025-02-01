1bc136f89398f9ab27eadea6bda83c5354f4dcee
Ricardo Branco <rbranco@suse.de>
2025-02-01 22:19:37 +0100

    Update dependencies

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module restartable
 go 1.22
 
 require (
-	github.com/spf13/pflag v1.0.5
-	golang.org/x/sys v0.28.0
+	github.com/spf13/pflag v1.0.6
+	golang.org/x/sys v0.29.0
 )
--- restartable.go
+++ restartable.go
@@ -20,7 +20,7 @@ import (
 
 import flag "github.com/spf13/pflag"
 
-const version string = "2.3.4"
+const version = "2.3.4"
 
 // ProcFS defines an interface for /proc/ filesystem access
 type ProcFS interface {
